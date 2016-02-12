5dae27622420dd215d68c084ba0995e268d93cb9
Antonio de Luna Lopez <tonydelun@gmail.com>
2016-02-12 13:20:49 +0000

    listo

--- prac1.vhd
+++ prac1.vhd
@@ -13,19 +13,29 @@ entity bcd_adder_subs is
 end bcd_adder_subs;
 
 architecture behavior_bcdAS of bcd_adder_subs is
-  signal c: unsigned (3 downto 0);
+  signal c: unsigned (4 downto 0);
   signal d: unsigned (7 downto 0);
   signal e: unsigned (7 downto 0);
   signal f: unsigned (7 downto 0);
+  signal g: unsigned (7 downto 0);
+  signal n: std_logic;
 begin
   -- complemento 2 si R = 1
-  c <= (B xor (3 downto 0 => R)) + ('0' & R);
+  c <= ('0'&(B xor (3 downto 0 => R)) + ('0' & R));
   -- suma
-  d <= ("0000" & A) + ("0000" & c);
+  d <= ("0000" & A) + ("000" & c);
 
   -- es negativo?
-  neg <= (R and (not d(4)));
-  -- si es mayor o igual a 10 sumarle 6
-  e <= d + ("00000110" and (7 downto 0 => (d(4) or (d(3) and (d(2) or (d(1)))))));
-  S(7 downto 0) <= e;
+  n <= (R and (not d(4)));
+  neg <= n;
+  -- si es mayor o igual a 10 sumarle 6 si no fue resta
+  e <= d + ("00000110" and (7 downto 0 => ((not R) and (d(4) or (d(3) and (d(2) or (d(1))))))));
+
+  -- si es resta ignorar el bit de carry
+  f <= e xor ("00010000" and (7 downto 0 => (R and not n)));
+
+  --si es negativo complemento 2
+  g <= (f xor "0000"&(3 downto 0 => n)) + ('0' & n); 
+
+  S(7 downto 0) <= g;
 end behavior_bcdAS;
--- prac1_tb.vhd
+++ prac1_tb.vhd
@@ -20,7 +20,7 @@ architecture beh of bcd_tb is
   signal neg: std_logic;
 begin
   u0: bcd_adder_subs port map (A, B, R, S, neg);
-  A <= "1001" after 0 ns, "0110" after 10 ns, "0001" after 20 ns;
-  B <= "0011" after 0 ns;
-  R <= '0' after 0 ns, '1' after 5 ns, '0' after 10 ns, '1' after 15 ns, '0' after 20 ns, '1' after 25 ns, '0' after 30 ns;
+  A <= "1001" after 0 ns, "0110" after 10 ns, "0001" after 20 ns, "0000" after 30 ns;
+  B <= "1001" after 0 ns, "0000" after 30 ns;
+  R <= '0' after 0 ns, '1' after 5 ns, '0' after 10 ns, '1' after 15 ns, '0' after 20 ns, '1' after 25 ns, '0' after 30 ns, '1' after 35 ns, '0' after 40 ns, '1' after 45 ns, '0' after 50 ns;
 end beh;
